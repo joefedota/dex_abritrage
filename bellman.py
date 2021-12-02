26222195727cca3705d7756bb58dc28a1820cc58
Jake <jake.scigliano@berkeley.edu>
2021-12-02 13:36:03 -0800

    added randomized speedup algorithm

--- bellman.py
+++ bellman.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import random
 
 class NegativeWeightFinder:
     __slots__ = ['graph', 'predecessor_to', 'distance_to', 'seen_nodes']
@@ -29,7 +30,7 @@ class NegativeWeightFinder:
         self.distance_to[source] = 0
 
 
-    def bellman_ford(self, source='BTC', unique_paths=True):
+    def bellman_ford(self, source='WETH', unique_paths=True):
         """
         Finds arbitrage opportunities in self.graph and yields them
         Parameters
@@ -100,6 +101,87 @@ class NegativeWeightFinder:
 
             arbitrage_loop.insert(0, prior_node)
             self.seen_nodes.add(prior_node)
+
+
+
+
+
+# Speedup
+
+    def bellman_ford_randomized_speedup(self, source='WETH', unique_paths=True):
+        # Initialize our graph weights
+        self.initialize(source)
+
+        # Create a dictionary for node to value pairings, as well as value to node
+        # pairings for easy numerical ordering
+        node_values, value_nodes = self.randomized_nodes()
+        
+        # Keep track of nodes where D[node] was changed in the previoius iteration.
+        # Once no nodes have been changed in the previous iteration, the algorithm is
+        # completed.
+        changed_list = set(source)
+        while changed_list:
+
+            # We need to keep track of node v in any (u, v) edges where D[v] is updated
+            # in the current iteration
+            changed_list_neighbors = set()
+
+            # For each node in ASCENDING numerical order, relax any 
+            # edge (u, v) where v holds a greater node value than u
+            # only if u is in the changed list, or D[v] has been changed
+            # since the start of the iteration
+            for i in range(self.graph.num_of_nodes()):
+                u = value_nodes[i]
+                u_value = node_values[u]
+                for v in self.graph[u]:
+                    if u in changed_list or v in changed_list_neighbors:
+                        if node_values[v] > u_value:
+                            weight = self.graph.edges[u, v]['weight']
+                            if self.relax(u, v, weight):
+                                changed_list_neighbors.add(v)
+
+            # For each node in DECREASING numerical order, relax any
+            # edge (u, v) where v holds a lesser node value than u
+            # only if u is in the changed list, or D[v] has been changed
+            # since the start of the iteration
+            for i in range(self.graph.num_of_nodes()-1, -1, -1):
+                u = value_nodes[i]
+                u_value = node_values[u]
+                for v in self.graph[u]:
+                    if u in changed_list or v in changed_list_neighbors:
+                        if node_values[v] < u_value:
+                            weight = self.graph.edges[u, v]['weight']
+                            if self.relax(u, v, weight):
+                                changed_list_neighbors.add(v)
+        
+        # Carry on like regular Bellman-Ford, completing one more pass to detect negative cycles
+        for edge in self.graph.edges(data=True):
+            if self.distance_to[edge[0]] + edge[2]['weight'] < self.distance_to[edge[1]]:
+                if unique_paths and edge[1] in self.seen_nodes:
+                    continue
+                path = self._retrace_negative_cycle(edge[1], unique_paths)
+                if path is None or path == (None, None):
+                    continue
+                yield path
+    
+        return None
+
+    def randomized_nodes(self):
+        num_of_nodes = self.graph.number_of_nodes()
+
+        nodes = list(self.graph.nodes)
+        numbers = [i for i in range(num_of_nodes)]
+        random.shuffle(numbers)
+
+        # Return a dictionary of node : node value key/value pairs
+        return dict(zip(nodes, numbers)), dict(zip(numbers, nodes))
+
+    def relax(self, node, neighbor, weight):
+        if self.distance_to[node] + weight < self.distance_to[neighbor]:
+            self.distance_to[neighbor] = self.distance_to[node] + weight
+            self.predecessor_to[neighbor] = node
+            return True
+        return False
         
 def last_index_in_list(lst, element):
     return len(lst) - next(i for i, v in enumerate(reversed(lst), 1) if v == element)
