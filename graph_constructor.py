10fa45ac4218d8dd48c22945820378212fc92d68
Joe Fedota <joefedota@berkeley.edu>
2021-12-01 21:04:16 -0800

    support added for uniswapv3/minor redundancy checks

--- graph_constructor.py
+++ graph_constructor.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from uniswap import uni_sushi_swap, kyber, dydx
+from uniswap import uni_sushi_swap, kyber, dydx, univ3
 import pickle
 from math import log
 
@@ -8,6 +8,7 @@ def construct_graph(num_networks, load_state=False):
     sushi_tups = []
     kyber_tups = []
     dydx_tups = []
+    v3_tups = []
     #sushi_tups = uni_sushi_swap("https://api.thegraph.com/subgraphs/name/sushiswap/exchange")
     #uni_tups = uni_sushi_swap("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
     #kyber_tups = []
@@ -27,14 +28,29 @@ def construct_graph(num_networks, load_state=False):
             open_file = open("kyber.pkl", "rb")
             kyber_tups = pickle.load(open_file)
             open_file.close()
+        if num_networks > 4:
+            open_file = open("univ3.pkl", "rb")
+            v3_tups = pickle.load(open_file)
+            open_file.close()
     else:
+        print("loading sushi prices...")
         sushi_tups = uni_sushi_swap("https://api.thegraph.com/subgraphs/name/sushiswap/exchange")
+        print("DONE")
+        print("loading uni prices...")
         uni_tups = uni_sushi_swap("https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2")
+        print("DONE")
         if num_networks > 2:
+            print("loading dydx prices...")
             dydx_tups = dydx()
+            print("DONE")
         if num_networks > 3:
+            print("loading kyber prices...")
             kyber_tups = kyber()
-    
+            print("DONE")
+        if num_networks > 4:
+            print("loading univ3 prices...")
+            v3_tups = univ3()
+            print("DONE")
     
     graph = nx.MultiDiGraph()
     
@@ -53,28 +69,39 @@ def construct_graph(num_networks, load_state=False):
         graph.add_edge(tup[0], tup[1], weight=log(float(tup[3]))*(-1), market=tup[4])
         graph.add_edge(tup[1], tup[0], weight=(log(float(tup[2]))*(-1)), market=tup[4])
     if num_networks > 2:
+        
         for tup in dydx_tups:
             if not graph.__contains__(tup[0]):
                 graph.add_node(tup[0])
             if not graph.__contains__(tup[1]):
                 graph.add_node(tup[1])
-            graph.add_edge(tup[0], tup[1], weight=(log(float(tup[2]))*(-1)), market=tup[4])
+            graph.add_edge(tup[0], tup[1], weight=(log(float(tup[3]))*(-1)), market=tup[4])
             graph.add_edge(tup[1], tup[0], weight=(log(float(tup[2]))*(-1)), market=tup[4])
+
     if num_networks > 3:
+        
         for tup in kyber_tups:
             if not graph.__contains__(tup[0]):
                 graph.add_node(tup[0])
             if not graph.__contains__(tup[1]):
                 graph.add_node(tup[1])
-            graph.add_edge(tup[0], tup[1], weight=(log(float(tup[2]))*(-1)), market=tup[4])
+            graph.add_edge(tup[0], tup[1], weight=(log(float(tup[3]))*(-1)), market=tup[4])
             graph.add_edge(tup[1], tup[0], weight=(log(float(tup[2]))*(-1)), market=tup[4])
-    #pass the original value for the prices into the graph then have jakes 
-    # algo convert to negative logs in flight, that way can traverse the 
-    #discovered path to find profit
+    
+    if num_networks > 4:
+        for tup in v3_tups:
+            if not graph.__contains__(tup[0]):
+                graph.add_node(tup[0])
+            if not graph.__contains__(tup[1]):
+                graph.add_node(tup[1])
+            graph.add_edge(tup[0], tup[1], weight=(log(float(tup[3]))*(-1)), market=tup[4])
+            graph.add_edge(tup[1], tup[0], weight=(log(float(tup[2]))*(-1)), market=tup[4])
+
+    #this is to record the last market state if we just requeried it
+    if not load_state:
+        wts = graph.edges.data()
+        open_file = open("wts.txt", "w")
+        open_file.write(wts.__str__())
+        open_file.close()
+
     return graph
-'''
-g = construct_graph(4, load_state=False)
-wts = g.edges.data()
-open_file = open("wts.txt", "w")
-open_file.write(wts.__str__())
-open_file.close()'''
--- sushiswap.py
+++ sushiswap.py
@@ -1,39 +1,40 @@
 import requests
 import json
 
-endpoint = "https://api.thegraph.com/subgraphs/name/sushiswap/exchange"
+endpoint = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
 headers = {}
-
-query = """{
-   pairs(first: 1000, orderBy: reserveUSD, orderDirection: desc) {
-     id
-   }
- }"""
-
-r = requests.post(endpoint, json={"query": query}, headers=headers)
-
-data_json = json.loads(r.text)["data"]
-
-data = data_json['pairs']
-responses = []
-for pair in data:
+tuples = []
+skip = 0
+while True:
+    print(skip)
     query = """{
-   pair(id: \"""" + pair['id'] + """\"){
-     token0 {
-       id
-       symbol
-       name
-       derivedETH
-     }
-     token1 {
-       id
-       symbol
-       name
-       derivedETH
-     }
-     token0Price
-     token1Price
-   }
-   }"""
-    responses.append(requests.post(endpoint, json={"query": query}, headers=headers))
-print(responses[0].text)
+    pools(first: 1000, skip: """ + str(skip) + """) {
+        id
+        token0 {
+            id
+            symbol
+            name
+            derivedETH
+        }
+        token1 {
+            id
+            symbol
+            name
+            derivedETH
+        }
+        token0Price
+        token1Price
+        }
+    }"""
+    r = requests.post(endpoint, json={"query": query}, headers=headers)
+    dic = json.loads(r.text)
+    #print(dic)
+    #print(dic['data']['pairs'])
+    if 'data' in dic:
+        if len(dic['data']) == 0:
+            break
+        for pair in dic['data']['pools']:
+            tuples.append((pair['token0']['symbol'], pair['token1']['symbol'], pair['token0Price'], pair['token1Price'], "univ3"))
+    else:
+        break
+    skip += 1000
